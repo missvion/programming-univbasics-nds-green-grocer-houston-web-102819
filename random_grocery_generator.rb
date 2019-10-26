be07818334df7b92f5b19de24998ebff301da999
Vivian Tran <viviantran94@gmail.com>
2019-10-26 17:16:49 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -1,57 +1,53 @@
-require_relative 'grocer'
-
-def items
-	[
-		{"AVOCADO" => {:price => 3.00, :clearance => true}},
-		{"KALE" => {:price => 3.00, :clearance => false}},
-		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-		{"ALMONDS" => {:price => 9.00, :clearance => false}},
-		{"TEMPEH" => {:price => 3.00, :clearance => true}},
-		{"CHEESE" => {:price => 6.50, :clearance => false}},
-		{"BEER" => {:price => 13.00, :clearance => false}},
-		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-		{"BEETS" => {:price => 2.50, :clearance => false}}
-	]
+def consolidate_cart(cart)
+  organized_cart = {}
+  count = 0
+  cart.each do |element|
+    element.each do |fruit, hash|
+      organized_cart[fruit] ||= hash
+      organized_cart[fruit][:count] ||= 0
+      organized_cart[fruit][:count] += 1
+    end
+  end
+  return organized_cart
 end
 
-def coupons
-	[
+def apply_coupons(cart, coupons)
+  coupons.each do |coupon_hash|
+    fruit_name = coupon_hash[:item]
+    new_coupon_hash = {
+      :price => coupon_hash[:cost],
+      :clearance => "true",
+      :count => coupon_hash[:num]
+    }
+    
+     if cart.key?(fruit_name)
+      new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
+      if cart[fruit_name][:count]>= new_coupon_hash[:count]
+        new_coupon_hash[:count] = (cart[fruit_name][:count]/new_coupon_hash[:count]).floor
+        cart[fruit_name][:count] = (coupon_hash[:num])%(cart[fruit_name][:count])
+      end
+      cart[fruit_name + " W/COUPON"] = new_coupon_hash 
+    end
+    end
+  return cart
+end
+
+test_cart = {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 4},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
+
+test_coupon = 
+[
 		{:item => "AVOCADO", :num => 2, :cost => 5.00},
 		{:item => "BEER", :num => 2, :cost => 20.00},
 		{:item => "CHEESE", :num => 3, :cost => 15.00}
 	]
-end
+	
+result = 
+{
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 2},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
 
-def generate_cart
-	[].tap do |cart|
-		rand(20).times do
-			cart.push(items.sample)
-		end
-	end
-end
-
-def generate_coupons
-	[].tap do |c|
-		rand(2).times do
-			c.push(coupons.sample)
-		end
-	end
-end
-
-cart = generate_cart
-coupons = generate_coupons
-
-puts "Items in cart"
-cart.each do |item|
-	puts "Item: #{item.keys.first}"
-	puts "Price: #{item[item.keys.first][:price]}"
-	puts "Clearance: #{item[item.keys.first][:clearance]}"
-	puts "=" * 10
-end
-
-puts "Coupons on hand"
-coupons.each do |coupon|
-	puts "Get #{coupon[:item].capitalize} for #{coupon[:cost]} when you by #{coupon[:num]}"
-end
-
-puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
+apply_coupons(test_cart, test_coupon)
